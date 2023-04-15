63ff4faf4d453f54c457cfd20aebde0846ad4724
varunraokadaparthi <varunraok123@gmail.com>
2023-04-15 13:37:05 -0400

    Session Management

---
+++ model/UserModel.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.fist_name = first_name
+        self.last_name = last_name
+
--- routes/profile.py
+++ routes/profile.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, Blueprint
-from db import db, cursor
+from flask import Flask, session, render_template, request, redirect, url_for, Blueprint
+from db import db
 
 profile_bp = Blueprint("profile_bp", __name__)
 
 
 @profile_bp.route("/profile")
 def profile():
-    user_id = request.args.get("user_id")
+    user_id = session["user_id"]
     # # insert the new user into the database
     # query = "INSERT INTO NUser (first_name, last_name, phone_number, age, gender, email_id, username, upassword, hint, languages_known) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     # cursor.execute(query, (first_name, last_name, phone_number, age, gender, email_id, username, upassword, hint, languages_known))
     # db.commit()
-    return render_template("profile.html", user_id=user_id)
+    return render_template("profile.html")
